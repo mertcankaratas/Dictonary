ec31cd1c6daf95d03281b01ee2baa07004b58a48
Mertcan Karatas <can.mert.karatas@gmail.com>
2021-07-13 18:58:14 +0300

    Proje dosyası ekle.

---
+++ Dictonary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,V>
+    
+        {
+        T[] items;
+        V[] values;
+        //constructor 
+        public MyDictionary()
+        {
+            items = new T[0];
+            values = new V[0];
+        }
+        public void Add(T item,V value)
+        {
+            T[] tempArrayKey = items;
+            V[] tempArrayValue = values;
+            items = new T[items.Length + 1];
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                items[i] = tempArrayKey[i];
+                values[i] = tempArrayValue[i];
+            }
+            items[items.Length - 1] = item;
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public void WriteConsoleListValue()
+        {
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+
+        public void WriteConsoleListKey()
+        {
+            foreach (var key in items)
+            {
+                Console.WriteLine(key);
+            }
+        }
+    }
+}
+    
+
---
+++ Dictonary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> isimler = new MyDictionary<int, string>();
+            isimler.Add(111,"Mertcan");
+            Console.WriteLine(isimler.Length);
+            isimler.Add(122,"Mert");
+
+            Console.WriteLine(isimler.Length);
+            isimler.WriteConsoleListValue();
+            isimler.WriteConsoleListKey();
+        }
+    }
+}
